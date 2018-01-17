2267af82cca6ff99541bdbe0c840218e488977c0
yhylord <yhylord@gmail.com>
2018-01-17 17:53:21 +0800

    Add rocket launch

--- run.py
+++ run.py
@@ -17,31 +17,48 @@ gc.queue_research(bc.UnitType.Rocket)
 gc.queue_research(bc.UnitType.Rocket)
 
 my_team = gc.team()
+replicate_prob = 1/3
+mars_map = gc.starting_map(bc.Planet.Mars)
+mars_height = mars_map.height
+mars_width = mars_map.width
+
+
+def get_random_mars_position():
+    row = random.randint(0, mars_height)
+    column = random.randint(0, mars_width)
+    return bc.MapLocation(bc.Planet.Mars, row, column)
+
 
 while True:
     # We only support Python 3, which means brackets around print()
     print('pyround:', gc.round(), 'time left:', gc.get_time_left_ms(), 'ms')
 
     units = gc.my_units()
-    for unit in units:
-        type = unit.unit_type
-        d = random.choice(directions)
-        if type == bc.UnitType.Worker:
-            if gc.karbonite() > bc.UnitType.Rocket.blueprint_cost() and gc.can_blueprint(unit.id, bc.UnitType.Rocket, d):
-                gc.blueprint(unit.id, bc.UnitType.Rocket, d)
-            d2 = random.choice(directions)
-            if gc.can_replicate(unit.id, d2):
-                gc.replicate(unit.id, d2)
-            d3 = random.choice(directions)
-            if gc.can_harvest(unit.id, d3):
-                gc.harvest(unit.id, d3)
-            location = unit.location
-            if location.is_on_map():
-                nearby = gc.sense_nearby_units(location.map_location(), 2)
-                for other in nearby:
-                    if gc.can_build(unit.id, other.id):
-                        gc.build(unit.id, other.id)
-
+    try:
+        for unit in units:
+            type = unit.unit_type
+            d = random.choice(directions)
+            if type == bc.UnitType.Worker:
+                if gc.karbonite() > bc.UnitType.Rocket.blueprint_cost() and gc.can_blueprint(unit.id, bc.UnitType.Rocket, d):
+                    gc.blueprint(unit.id, bc.UnitType.Rocket, d)
+                d2 = random.choice(directions)
+                if gc.can_replicate(unit.id, d2) and random.random() < replicate_prob:
+                    gc.replicate(unit.id, d2)
+                d3 = random.choice(directions)
+                if gc.can_harvest(unit.id, d3):
+                    gc.harvest(unit.id, d3)
+                location = unit.location
+                if location.is_on_map():
+                    nearby = gc.sense_nearby_units(location.map_location(), 2)
+                    for other in nearby:
+                        if gc.can_build(unit.id, other.id):
+                            gc.build(unit.id, other.id)
+            elif type == bc.UnitType.Rocket:
+                if not unit.rocket_is_used() and unit.structure_is_built():
+                    destination = get_random_mars_position()
+                    gc.launch_rocket(unit.id, destination)
+    except Exception as e:
+        print(e)
 
     # send the actions we've performed, and wait for our next turn.
     gc.next_turn()
